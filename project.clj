ec5b550ba235ebdf8af8a699666371cadb90df9b
Alexander Kellett <a@lypanov.net>
2012-05-26 09:27:44 +0200

    try that again...

--- project.clj
+++ project.clj
@@ -1,10 +1,12 @@
-(defproject noir-cljs-lypanov "0.3.3"
+(defproject noir-cljs-lypanov "0.3.4"
   :description "A noir utility to add CLJS compiling through middleware"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [noir "1.3.0-beta2"]
                  [fetch "0.1.0-alpha2"]
+                 [hiccup "1.0.0"]
                  [jayq "0.1.0-alpha1"]
                  [crate "0.2.0-alpha2"]
                  [watchtower "0.1.1"]
                  [colorize "0.1.1"]
-                 [org.clojure/clojurescript "0.0-1236"]])
+                 [org.clojure/clojurescript "0.0-1236"]
+                 ])
