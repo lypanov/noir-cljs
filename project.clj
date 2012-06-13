f11efbed622b53be88ef645fee9c2dd48769b414
Alexander Kellett <a@lypanov.net>
2012-06-13 23:50:28 +0200

    new local 0.3.5 release with watcher changes

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject noir-cljs-lypanov "0.3.4"
+(defproject noir-cljs-lypanov "0.3.5"
   :description "A noir utility to add CLJS compiling through middleware"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [noir "1.3.0-beta2"]
